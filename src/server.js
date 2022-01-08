e4bf9ed29c7ea9c8f52d3bf0bbaf127163e94900
niyozneo <niyoz2005@gmail.com>
2022-01-08 16:04:28 +0500

    DGRD

--- src/server.js
+++ src/server.js
@@ -6,7 +6,11 @@ const { PORT } = require("./config/config");
 const cors = require("cors");
 const routes = require("./modules/routes");
 
-app.use(express.json());
+app.use(express.json({
+  origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}));
 
 app.use(cors());
 app.use(routes);
