8671f650be63f1e6098ccf54a4836403571be059
niyozneo <niyoz2005@gmail.com>
2022-01-08 16:04:55 +0500

    ADAW

--- src/server.js
+++ src/server.js
@@ -6,13 +6,13 @@ const { PORT } = require("./config/config");
 const cors = require("cors");
 const routes = require("./modules/routes");
 
-app.use(express.json({
+app.use(express.json());
+
+app.use(cors({
   origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }));
-
-app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => console.log(PORT));
